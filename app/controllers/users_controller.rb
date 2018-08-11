072f5076cade6b8cddd633413132d5fccb8ff91e
faxathome <parafajardo@gmail.com>
2018-08-11 16:10:03 +0200

    Add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
-  
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -13,31 +17,31 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
      @user = User.find(params[:id])
-     
+
      if @user.update(user_params)
        flash[:success] = "Your account was updated successfully"
        redirect_to articles_path
      else
        render 'edit'
      end
-     
+
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
-  
+
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
+
 end
